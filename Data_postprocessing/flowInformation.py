7461057e55315fe680dbda2c1fec19cb7a4f5843
andres2631996 <47141286+andres2631996@users.noreply.github.com>
2020-03-02 11:16:50 +0100

    Add files via upload

---
+++ Data_postprocessing/flowInformation.py
@@ -0,0 +1,490 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 14:32:46 2020
+
+@author: andres
+"""
+
+import numpy as np
+
+import scipy.io
+
+import os
+
+import vtk
+
+import matplotlib.pyplot as plt
+
+from vtk.util.numpy_support import numpy_to_vtk, vtk_to_numpy
+
+# Code to extract flow information from ROIs in MAT files
+
+# Compare results when I have segmentations from neural networks
+
+# Export flow info in TXT files
+
+
+
+# Extract results from neural network masks
+
+
+class FlowInfo:
+    
+    """
+    Extract or load flow information from MAT files or masks.
+    
+    Params:
+        
+        - study: study where files belong to
+        
+        - path: path of mask or MAT file for saving, path of TXT file for loading
+        
+        - dest_path: path of TXT file for saving, None for loading
+        
+        - raw_file_path: path of raw file. Only for saving from mask information. Otherwise is None
+        
+        - flag: either 'save' for saving or 'load' for loading
+        
+        - energy: in case of flow info loading, save the energy if available
+        
+        - load_file: TXT file to read in case of file loading 
+    
+    Returns:
+        
+        - if flag is 'save', saves flow information as TXT in location specified
+        
+        - if flag is 'load', loads flow information from TXT and provides arrays
+    
+    """
+    
+    def __init__(self, study, path, dest_path, raw_file_path, flag, energy, file):
+        
+        self.study = study
+        
+        self.path = path
+        
+        self.dest_path = dest_path
+        
+        self.raw_file_path = raw_file_path
+
+        self.flag = flag
+        
+        self.energy = energy
+        
+        self.load_file = file
+        
+
+    def readVTK(self, path, filename, order='F'):
+            
+        """
+        Utility function to read vtk volume. 
+        
+        Params:
+            
+            - inherited from class (check at the beginning of the class)
+            - path: path where VTK file is located
+            - filename: VTK file name
+        
+        Returns:
+            
+            - numpy array
+            - data origin
+            - data spacing
+        
+        """
+    
+        reader = vtk.vtkStructuredPointsReader()
+    
+        reader.SetFileName(path + filename)
+    
+        reader.Update()
+    
+        image = reader.GetOutput()
+    
+        numpy_array = vtk_to_numpy(image.GetPointData().GetScalars())
+    
+        numpy_array = numpy_array.reshape(image.GetDimensions(),order='F')
+    
+        numpy_array = numpy_array.swapaxes(0,1)
+    
+        origin = list(image.GetOrigin())
+    
+        spacing = list(image.GetSpacing())
+    
+        return numpy_array, origin, spacing
+
+
+
+    def flowFromTXT(self):
+        
+        """
+        Gets numpy array from TXT file with flow information.
+        
+        Params:
+            
+            - inherited from class (check at the beginning of the class)
+            - file: TXT file
+        
+        Returns:
+            
+            - flow parameters
+        
+        """
+    
+        
+        array = np.loadtxt(self.path + self.load_file)
+        
+        if self.energy:
+            
+            mean_v = array[0,:]
+        
+            std_v = array[1,:]
+            
+            max_v = array[2,:]
+            
+            min_v = array[3,:]
+            
+            energy = array[4,:]
+            
+            area = array[5,:]
+        
+            net_flow = array[6,:]
+            
+            pos_flow = array[7,:]
+            
+            neg_flow = array[8,:]
+            
+            return mean_v, std_v, max_v, min_v, energy, area, net_flow, pos_flow, neg_flow
+        
+        else:
+        
+            mean_v = array[0,:]
+            
+            std_v = array[1,:]
+            
+            max_v = array[2,:]
+            
+            min_v = array[3,:]
+    
+            area = array[4,:]
+            
+            net_flow = array[5,:]
+            
+            pos_flow = array[6,:]
+            
+            neg_flow = array[7,:]
+            
+            return mean_v, std_v, max_v, min_v, area, net_flow, pos_flow, neg_flow
+    
+
+    def flowFromMask(self, mask, raw, spacing):
+        
+        """
+        Compute flow parameters from masks and phase images.
+        
+        Params:
+            
+            - inherited from class (check at the beginning of the class)
+            - mask: binary or quasi-binary 3D array with results from neural network segmentation
+            - raw: corresponding phase image
+            - spacing: pixel size, useful to extract area and flow information
+        
+        Returns:
+            
+            - result: 2D array with results on average velocity, standard deviation,
+                    maximum velocity, minimum velocity, area, net flow, positive flow
+                    and negative flow
+        
+        """
+        
+        # Multiply mask to raw phase image: get just ROI information
+        
+        mult = mask * raw
+        
+        result = np.zeros((8, mask.shape[2]))
+        
+        for j in range(mask.shape[2]): # Frame by frame analysis
+            
+            mult_frame = mult[:,:,j] # Frame of the multiplication operator
+            
+            s = np.sum(mult_frame.flatten()) # Overall sum of each frame. If > 0 --> left kidney, if < 0 --> right kidney
+            
+            ind = np.where(mult_frame != 0) # Indexes inside the ROI
+            
+            result[0,j] = np.abs(np.mean(mult_frame[ind].flatten()))/100 # Mean values
+            
+            result[1,j] = np.abs(np.std(mult_frame[ind].flatten()))/100 # Standard deviation
+            
+            result[2,j] = np.amax(mult_frame)/100 # Maximum value
+            
+            result[3,j] = np.amin(mult_frame)/100 # Minimum value
+            
+            result[4,j] = np.array(ind).shape[-1]*spacing[0]*spacing[1]/100 # Area
+    
+            result[5,j] = abs(s/result[4,j])/10000 # Net flow
+            
+            ind_pos = np.where(mult_frame > 0) # Positive voxel values
+                
+            ind_neg = np.where(mult_frame < 0) # Negative voxel values
+            
+            if s > 0:
+        
+                result[6,j] = np.sum(mult_frame[ind_pos].flatten())/(result[4,j]*10000) # Positive flow values
+                
+                if len(ind_neg) != 0: # Check if there are any negative voxels
+                    
+                    result[7,j] = np.sum(mult_frame[ind_neg].flatten())/(result[4,j]*10000) # Negative flow values
+                
+                else:
+                    
+                    result[7,j] = 0
+            
+            elif s < 0:
+                
+                result[6,j] = np.sum(mult_frame[ind_neg].flatten())/(result[4,j]*10000) # Positive flow values
+                
+                if len(ind_pos) != 0: # Check if there are any positive voxels
+                    
+                    result[7,j] = np.sum(mult_frame[ind_pos].flatten())/(result[4,j]*10000) # Negative flow values
+                
+                else:
+                    
+                    result[7,j] = 0
+                
+        return result
+
+
+
+    def mat_loader(self, filename):
+            
+        """
+        Load .mat file from Segment software
+        
+        Params:
+            
+            - inherited from class (check at the beginning of the class)
+            
+            - path and filename, inherited from the class
+        
+        Returns:
+            
+            - mat: dictionary with .mat file fields
+            
+            - st: structure field with data
+            
+        """
+        
+        #print('Conversion from .mat into array\n')
+        
+        mat = scipy.io.loadmat(self.path + filename) 
+    
+        st = mat.get('setstruct') 
+        
+        return mat, st
+
+
+
+    def flowFromMAT(self, filename):
+        
+        """
+        Get flow information from measurements saved in MAT files. 
+        
+        These measurements are considered to be the ground truth measurements for flow by software
+        
+        Params:
+            
+            - inherited from class (check at the beginning of the class)
+            
+            - path: folder with MAT files of interest
+            
+            - filename: MAT filename
+        
+        """
+        
+        
+
+        
+            
+        mat, st = self.mat_loader(filename)
+        
+        img = st[0,0][0]
+
+        res = np.zeros((10, img.shape[-1]))
+
+        for j in range(len(st[0,0][53][0,0][7][0])):
+            
+            # See if there is flow information from more than one ROI
+            
+            cont = 0
+        
+            cont_correct = 0
+
+            while cont_correct < (res.shape[0] - 1):
+
+                if st[0,0][53][0,0][7][0,j][cont].shape[1] == res.shape[1]:
+
+                    res[cont_correct,:] += np.reshape(st[0,0][53][0,0][7][0,j][cont], res[cont,:].shape)
+                    
+                    if cont_correct != res.shape[1] - 1:
+                    
+                        cont_correct += 1
+                    
+                cont += 1
+
+            return res[:-1,:]
+    
+
+
+    def __main__(self):
+        
+        if (self.flag == 'save') or (self.flag == 'Save') or (self.flag == 'SAVE'):
+            
+            # Save flow info from VTK masks or MAT files
+
+            if self.raw_file_path is not None: # Save info from VTK masks
+                
+                raw_files = os.listdir(self.raw_file_path)
+                
+                # Load phase images' information
+                
+                raw_info = []
+
+                for raw_file in raw_files:
+                    
+                    raw_info.append(raw_file[0:20] + raw_file[24:25]) # List with info from raw files
+                
+                raw_info_array = np.array(raw_info) # List into array
+                
+                # Load mask information
+
+                mask_files = os.listdir(self.path)
+                
+                mask_info = []
+                
+                for mask_file in mask_files:
+
+                    mask, _, _ = self.readVTK(self.path, mask_file) # VTK mask loading
+                
+                    mask_info = mask_file[0:20] + mask_file[24:25]
+                    
+                    ind = np.where(raw_info_array == mask_info)
+                
+                    for i in ind:
+                        
+                        raw, origin, spacing = self.readVTK(self.raw_file_path, raw_files[i])
+                
+                        ##       PERHAPS SOME SCALING PARAMETER NEEDS TO BE INTRODUCED!!!!
+#
+                        res = self.flowFromMask(mask, raw, spacing)
+                
+                        # Save flow information as txt
+        
+                        np.savetxt(self.dest_path + raw_info[i] + '_flowInfo.txt', res)
+                        
+                
+            
+            else: # Save info from MAT files
+                
+                mat_files = sorted(os.listdir(self.path))
+                
+                for mat_file in mat_files:
+    
+                    if mat_file[-3:] == 'mat': # Access only MAT files
+                        
+                        res = self.flowFromMAT(mat_file)
+                        
+                        if self.study == 'CKD2':
+
+                            ind = mat_file.index('.')
+                            
+                            ind_aux = mat_file.index('^')
+                            
+                            filename_save = self.dest_path + self.study + '_' + mat_file[:ind_aux] + '_' + mat_file[(ind_aux + 1): (ind_aux + 5)] + mat_file[-9:-4] + 'flowInfo.txt'
+                            
+                        elif self.study == 'Hero' or self.study == 'Extr':
+                            
+                            filename_save = self.dest_path + mat_file[:-4] + '_flowInfo.txt'
+
+                        np.savetxt(filename_save, res)
+                        
+                        
+        
+        elif (self.flag == 'load') or (self.flag == 'Load') or (self.flag == 'LOAD'):
+            
+            # Load flow info from TXT files that have been previously saved
+
+            if self.energy:
+                
+                mean_v, std_v, max_v, min_v, energy, area, net_flow, pos_flow, neg_flow = self.flowFromTXT() # Just an example!!
+                
+                return mean_v, std_v, max_v, min_v, energy, area, net_flow, pos_flow, neg_flow
+                
+            else:
+                
+                mean_v, std_v, max_v, min_v, area, net_flow, pos_flow, neg_flow = self.flowFromTXT() # Just an example!!
+                
+                return mean_v, std_v, max_v, min_v, area, net_flow, pos_flow, neg_flow
+            
+            # DO FURTHER ACTION IN HERE
+ 
+
+
+                       
+# Some test code...
+
+   
+# Get directory of phase images and different phase images
+            
+study = 'Extr'
+
+raw_path = '/home/andres/Documents/_Data/CKD_Part2/3_Clean_Data/_pha/'
+
+mask_path = '/home/andres/Documents/_Data/Extra/_Binary_masks/'
+
+save_path = '/home/andres/Documents/_Data/Extra/_Flow/'
+
+gt_path = '/home/andres/Documents/_Data/Extra/_Test_measurements_Andres/' # Folder for ground truths
+
+#txt_files = os.listdir(save_path)
+
+# For saving info from VTK masks:
+
+# save_info_mask = FlowInfo(mask_path, save_path, raw_path, 'save', None)
+
+# For saving info from MAT files:
+
+save_info_mat = FlowInfo(study, gt_path, save_path, None, 'save', None, None)
+
+# For loading info from VTK masks that has been previously saved as TXT:
+
+# load_info_mask = FlowInfo(save_path, None, None, 'load', None)
+
+# For saving info from MAT files that has been previously saved as TXT:
+
+#load_info_mat = FlowInfo(save_path, None, None, 'load', True, txt_files[0])
+
+# save_info_mask.__main__()
+
+save_info_mat.__main__()
+
+# mean_v, std_v, max_v, min_v, area, net_flow, pos_flow, neg_flow = load_info_mask.__main__()
+
+#mean_v, std_v, max_v, min_v, energy, area, net_flow, pos_flow, neg_flow = load_info_mat.__main__()
+
+#plt.figure()
+
+#plt.plot(np.arange(net_flow.shape[0]), net_flow)
+
+
+# To run in terminal:
+
+# mask_path = sys.argv[1]
+# save_path = sys.argv[2] 
+# raw_path = sys.argv[3]
+# flag = sys.argv[4] 
+# energy = sys.argv[5]
+    
+
+
+# if __name__ == "__main__":  
---
+++ Data_postprocessing/flowStatistics.py
@@ -0,0 +1,344 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 08:35:58 2020
+
+@author: andres
+"""
+
+import numpy as np
+
+import scipy
+
+import os
+
+import matplotlib.pyplot as plt
+
+from sklearn import linear_model
+
+from sklearn.metrics import mean_squared_error, r2_score
+
+import statsmodels.api as sm
+
+
+
+# Code to deal with statistics in data post-processing
+
+def t_test(result, reference):
+    
+    """
+    Compute the t statistic and p-value given the flow results from the neural
+    network and the reference results from Segment.
+    
+    Params:
+        - result: 1D array with flow results from neural network segmentation
+        - reference: 1D array with reference flow results from software Segment
+    
+    Returns:
+        - t_statistic and p_value
+    
+    """
+    
+    # Check that result and reference are 1D and that they have the same length
+    
+    print('\nChecking that result and reference are 1D and that they have the same length\n')
+    
+    if (len(result.shape) == 1) and (len(reference.shape) == 1):
+        
+        if len(result) == len(reference):
+            
+            print('Performing t test\n')
+    
+            t_stat, p_value = scipy.stats.ttest_ind(result, reference)
+            
+            print('t test completed successfully!\n')
+            
+            print('t statistic: {} // p value: {}'.format(t_stat, p_value))
+            
+            return t_stat, p_value
+        
+        else:
+            
+            print('Result and reference vectors do not have the same length. Please input them so that they have the same length')
+        
+    else:
+        
+        print('Result or reference vectors are not 1D. Please reformat them to be 1D')
+        
+        
+
+
+def wilcoxon_test(result, reference):
+    
+    """
+    Compute the sum of rank differences and p-value given the flow results 
+    from the neural network and the reference results from Segment.
+    
+    Params:
+        - result: 1D array with flow results from neural network segmentation
+        - reference: 1D array with reference flow results from software Segment
+    
+    Returns:
+        - sum and p_value
+    
+    """
+    
+    print('\nChecking that result and reference are 1D and that they have the same length\n')
+    
+    if (len(result.shape) == 1) and (len(reference.shape) == 1):
+        
+        if len(result) == len(reference):
+            
+            print('Performing Wilcoxon test\n')
+            
+            s, p_value = scipy.stats.wilcoxon(result, reference)
+            
+            print('Wilcoxon test completed successfully!\n')
+            
+            print('Sum of rank differences: {} // p value: {}'.format(s, p_value))
+    
+            return s, p_value
+        
+        else:
+            
+            print('Result and reference vectors do not have the same length. Please input them so that they have the same length')
+        
+    else:
+        
+        print('Result or reference vectors are not 1D. Please reformat them to be 1D')
+        
+        
+
+def figure_saving(dest_path, filename, figure):
+    
+    """
+    Saves figure in specified folder with a given file name
+    
+    Params:
+        
+        - dest_path: destination folder
+        
+        - filename: given filename (must be .png)
+        
+        - figure: given figure from Matplotlib
+    
+    """
+    
+    print('Checking that a destination path has been given\n')
+    
+    if dest_path is not None:
+        
+        if filename[-3:] == 'png' or filename[-3:] == 'PNG':
+                        
+            print('Saving figure as PNG in: {}'.format(dest_path))
+            
+            # Check that folder exists. Otherwise, create it
+            
+            print('\nChecking if folder exists\n')
+            
+            exist = os.path.isdir(dest_path)
+            
+            if not exist:
+                
+                os.makedirs(dest_path)
+                
+                print('Non existing folder. Created\n')
+                
+            # Checking if file exists
+            
+            files = os.listdir(dest_path)
+    
+            if filename in files:
+                
+                inp = input('File already exists. Do you want to overwrite or rename or abort? [o/r/a]:')
+                
+                if (inp == 'o') or (inp == 'O'):
+                    
+                    figure.savefig(dest_path + filename)
+                
+                elif (inp == 'r') or (inp == 'R'):
+                    
+                    cont = 0 # Filename counter
+                    
+                    while (filename in files):
+                        
+                        filename = filename[:-4] + '_' + str(cont) + '.png'
+                        
+                        cont += 1
+                    
+                    figure.savefig(dest_path + filename)
+                    
+                    print('Figure successfully saved as PNG in {} after file renaming'.format(dest_path))
+                
+                else:
+                    
+                    print('Operation aborted\n')
+            else:
+        
+                figure.savefig(dest_path + filename)
+            
+                print('Figure successfully saved as PNG in {}'.format(dest_path))
+            
+        else:
+            
+            print('Filename given was not PNG. Please specify a PNG filename')
+    
+    else:
+        
+        print('Tried to save figure as PNG, but folder has not been specified')
+
+
+
+def linear_regression_test(result, reference, plotting = True, save = False, dest_path = os.getcwd() + '/', filename = 'regression_plot.png'):
+    
+    """
+    Compute a scatter plot with points and with linear regression equation
+    given the flow results from the neural network and the reference results 
+    from Segment.
+    
+    Params:
+        - result: 1D array with flow results from neural network segmentation
+        - reference: 1D array with reference flow results from software Segment
+        - plotting: flag to state whether the user wants to get a plot of the result or not (default True)
+        - save: flag indicating whether to save linear regression plot as .png file or not (default False)
+        - dest_path: folder where to save the linear regression plot if save is True (default current folder)
+        - filename: file name of regression plot (PNG file) (default: 'regression_plot.png')
+    
+    Returns:
+        - linear regression plot and linear regression parameters
+        - Return also squared error and R2
+    
+    """
+    
+    print('\nChecking that result and reference are 1D and that they have the same length\n')
+    
+    if (len(result.shape) == 1) and (len(reference.shape) == 1):
+        
+        if len(result) == len(reference):
+            
+            print('Computing linear regression model\n')
+    
+            # Create linear regression object
+            
+            regr = linear_model.LinearRegression()
+            
+            # Train the model
+            
+            # Add a column of ones to the reference
+            
+            ref_aux = np.ones((len(reference), 2))
+            
+            ref_aux[:,0] = reference
+            
+            regr.fit(ref_aux, result)
+    
+            # Make predictions using the reference 
+            
+            pred = regr.predict(ref_aux)
+            
+            print('Linear regression model completed successfully!\n')
+            
+            print('Resulting coefficients: {}\n'.format(regr.coef_))
+
+            # The mean squared error
+            
+            print('Mean squared error: {}\n'.format(mean_squared_error(result, pred)))
+            
+            # The coefficient of determination: 1 is perfect prediction
+            
+            print('Coefficient of determination: {}\n'.format(r2_score(result, pred)))
+            
+            if plotting:
+                
+                fig = plt.figure()
+                
+                # Points
+                
+                plt.scatter(result, reference, color = 'black', label = 'Measured points')
+                
+                # Regression line
+                
+                plt.plot(reference, pred, color='blue', linewidth=3, label = 'Regression line')
+                
+                plt.title('Linear regression plot')
+                
+                plt.xlabel('Reference values')
+                
+                plt.ylabel('Computed values')
+                
+                plt.legend()
+                
+                plt.xticks(())
+                
+                plt.yticks(())
+                
+                plt.show()
+                
+                if save:
+                    
+                    figure_saving(dest_path, filename, fig)
+                    
+            return regr.coef_, mean_squared_error(result, pred), r2_score(result, pred)
+            
+        else:
+            
+            print('Result and reference vectors do not have the same length. Please input them so that they have the same length')
+        
+    else:
+        
+        print('Result or reference vectors are not 1D. Please reformat them to be 1D')
+    
+
+
+
+def bland_altman_plot(result, reference, save = False, dest_path = os.getcwd() + '/', filename = 'bland_altman_plot.png'):
+    
+    """
+    Compute the Bland-Altman plot for the flow results from the neural network 
+    and the reference results from Segment.
+    
+    Params:
+        - result: 1D array with flow results from neural network segmentation
+        - reference: 1D array with reference flow results from software Segment
+        - save: flag indicating whether to save linear regression plot as .png file or not (default False)
+        - dest_path: folder where to save the linear regression plot if save is True (default current folder)
+        - filename: file name of regression plot (PNG file) (default bland_altman_plot.png)   
+        
+    
+    
+    """
+    
+    print('\nChecking that result and reference are 1D and that they have the same length\n')
+    
+    if (len(result.shape) == 1) and (len(reference.shape) == 1):
+        
+        if len(result) == len(reference):
+            
+            print('Computing Bland-Altman plot\n')
+            
+            f, ax = plt.subplots(1)
+            
+            sm.graphics.mean_diff_plot(result, reference, ax = ax)
+            
+            plt.title('Bland-Altman plot')
+                
+            plt.xlabel('Average values')
+            
+            plt.ylabel('Difference values')
+    
+            plt.show()
+    
+            if save:
+                    
+                figure_saving(dest_path, filename, f)
+            
+        else:
+            
+            print('Result and reference vectors do not have the same length. Please input them so that they have the same length')
+        
+    else:
+        
+        print('Result or reference vectors are not 1D. Please reformat them to be 1D')
+    
+    
+    
